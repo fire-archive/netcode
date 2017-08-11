20802964939dd7782d33bb74d838547831e1bc55
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-08-11 09:47:15 -0700

    Generate documentation  and include the standard.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Netcode.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+
+      #Docs
+      name: "Netcode",
+      source_url: "https://github.com/elixir-lang/ex_doc",
+      homepage_url: "https://github.com/elixir-lang/ex_doc",
+      docs: [main: "Netcode", # The main page in the docs
+#             logo: "path/to/logo.png",
+             extras: ["README.md", "markdown/STANDARD.md"]]
     ]
   end
 
@@ -21,6 +29,7 @@ defmodule Netcode.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:enacl, ~r/.*/, git: "https://github.com/jlouis/enacl.git", compile: "rebar compile"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
